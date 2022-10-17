c268001319b809f0bc71e6bced3c651bce0d715f
Masianuy <Masianuy@ukr.net>
2022-10-17 12:51:49 +0300

    deploy the whole page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,74 @@
-$(document).ready(function() {
-  $('#benefits_slider').owlCarousel({
-    loop:true,
-    margin:30,
-    autoWidth:false,
-    dots: false,
-    nav: true,
-    navText:["<div class='nav-btn prev-slide'><</div>","<div class='nav-btn next-slide'>></div>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
-        }
-    }
-  })
+
+
+$(document).ready(function(){
+  $('#main-slider').slick({
+    arrow: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    slidesToShow: 1,
+    // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    // nextArrow: '<button class="slide-arrow next-arrow"></button>'
+  });
 });
 
+$(document).ready(function(){
+  $('#benefits_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        },
+      }
+    ]
+  });
+});
+
+const mobMenu = document.getElementById('hamburger');
+const menuList = document.getElementById('nav');
+mobMenu.addEventListener('click', function() {
+  mobMenu.classList.toggle('close');
+    if (mobMenu.classList.contains('close')) {
+      menuList.style.transform = "translateX(0px)";
+    } else {
+      menuList.style.transform = "translateX(-320px)";
+    }
+})
+
+
+const tabCategory = document.querySelectorAll('.category_item');
+const categoriesContent = document.querySelectorAll('.category_content');
+
+  tabCategory.forEach(function(item) {
+    item.addEventListener('click' , function() {
+
+      const currentCategory = item;
+      const categoryName = currentCategory.getAttribute('data-category');
+      const currentContaier = document.getElementById(categoryName);
+
+      tabCategory.forEach(category => {
+        category.classList.remove('active');
+      })
+
+      categoriesContent.forEach(content => {
+        content.classList.remove('active');
+      })
+
+      currentCategory.classList.add('active');
+      currentContaier.classList.add('active');
+    })
+  })
